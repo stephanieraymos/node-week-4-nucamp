add7403f4624d6bdefa842f1f6a857d72ae27b9e
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-25 11:33:48 -0700

    Finalized post endpoint for /:campsiteId

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -7,99 +7,125 @@ const cors = require('./cors'); //CORS module created in the routes folder
 favoriteRouter.use(bodyParser.json());
 
 favoriteRouter.route('/')
-  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200)) //Handling preflight req. --> Any time a client needs to preflight a req: it will do so by sending a req with the http options method. Client will wait for server to respond with info on what kind of req it will accept to figure out whether or not it can send it's actual req.
-  .get(cors.cors, (req, res, next) => {
-    Favorite.find({ user: req.user._id }) //finding favorite by user id
-      .populate('user')
-      .populate('campsites')
-      .then(favorite => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(favorite);
-      })
-      .catch(err => next(err));
-  })
-  .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
-    Favorite.findOne({ user: req.user._id }) //finding each favorite
-      .then(favorite => {
-        if (favorite) {
-          req.body.forEach(favorite => {
-            if (!favorite.campsites.includes(favorite._id)) { //if no favorite id --> push favorite id
-              favorite.campsites.push(favorite._id);
-            }
-          });
-          favorite.save()
+    .options(cors.corsWithOptions, (req, res) => res.sendStatus(200)) //Handling preflight req. --> Any time a client needs to preflight a req: it will do so by sending a req with the http options method. Client will wait for server to respond with info on what kind of req it will accept to figure out whether or not it can send it's actual req.
+    .get(cors.cors, (req, res, next) => {
+        Favorite.find({ user: req.user._id }) //finding favorite by user id
+            .populate('user')
+            .populate('campsites')
             .then(favorite => {
-              res.statusCode = 200;
-              res.setHeader('Content-Type', 'application/json');
-              res.json(favorite);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(favorite);
             })
             .catch(err => next(err));
-        } else {
-          Favorite.create({ user: req.user._id, campsites: req.body })
+    })
+    .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        Favorite.findOne({ user: req.user._id }) //finding each favorite
             .then(favorite => {
-              res.statusCode = 200;
-              res.setHeader('Content-Type', 'application/json');
-              res.json(favorite);
+                if (favorite) {
+                    req.body.forEach(favorite => {
+                        if (!favorite.campsites.includes(favorite._id)) { //if no favorite id --> push favorite id
+                            favorite.campsites.push(favorite._id);
+                        }
+                    });
+                    favorite.save()
+                        .then(favorite => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(favorite);
+                        })
+                        .catch(err => next(err));
+                } else {
+                    Favorite.create({ user: req.user._id, campsites: req.body })
+                        .then(favorite => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(favorite);
+                        })
+                        .catch(err => next(err));
+                }
+            }).catch(err => next(err));
+    })
+    .put(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /favprites');
+    })
+    .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        Favorite.findOne({ user: req.user._id }) //finding each favorite
+            .then(favorite => {
+                if (favorite) { //if there's a favorite, remove it
+                    favorite.remove()
+                        .then(favorite => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(favorite);
+                        })
+                        .catch(err => next(err));
+                } else {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(favorite);
+                }
             })
             .catch(err => next(err));
-        }
-      }).catch(err => next(err));
-  })
-  .put(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /favprites');
-  })
-  .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
-    Favorite.findOne({ user: req.user._id }) //finding each favorite
-      .then(favorite => {
-        if (favorite) { //if there's a favorite, remove it
-          favorite.remove()
-            .then(favorite => {
-              res.statusCode = 200;
-              res.setHeader('Content-Type', 'application/json');
-              res.json(favorite);
-            })
-            .catch(err => next(err));
-        } else {
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-          res.json(favorite);
-        }
-      })
-      .catch(err => next(err));
-  });
+    });
 
 favoriteRouter.route('/:campsiteId')
-  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200)) //Handling preflight req. --> Any time a client needs to preflight a req: it will do so by sending a req with the http options method. Client will wait for server to respond with info on what kind of req it will accept to figure out whether or not it can send it's actual req.
-  .get(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
-    res.statusCode = 403;
-    res.end(`GET operation not supported on /favorites/${req.params.campsiteId}`);
-})
-  .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
-    res.statusCode = 403;
-    res.end(`POST operation not supported on /favorites/${req.params.campsiteId}`);
-  })
-  .put(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
-    Favorite.findByIdAndUpdate(req.params.campsiteId, {
-      $set: req.body //update operator along with the data in the request body
-    }, { new: true }) //this is so we get back info about the updated document resulting from this method
-      .then(favorite => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(favorite);
-      })
-      .catch(err => next(err));
-  })
-  .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
-    Favorite.findByIdAndDelete(req.params.campsiteId)
-      .then(response => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(response);
-      })
-      .catch(err => next(err));
-  });
+    .options(cors.corsWithOptions, (req, res) => res.sendStatus(200)) //Handling preflight req. --> Any time a client needs to preflight a req: it will do so by sending a req with the http options method. Client will wait for server to respond with info on what kind of req it will accept to figure out whether or not it can send it's actual req.
+    .get(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
+        res.statusCode = 403;
+        res.end(`GET operation not supported on /favorites/${req.params.campsiteId}`);
+    })
+    .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        Favorite.findOne({ user: req.user._id }) //finding favorite
+            .then(favorite => {
+                if (favorite) {
+                    if (!favorite.campsites.includes(req.params.campsiteId)) {
+                        favorite.campsites.push(req.params.campsiteId);
+                        favorite.save()
+                            .then(favorite => {
+                                res.statusCode = 200;
+                                res.setHeader('Content-Type', 'application/json');
+                                res.json(favorite);
+                            })
+                            .catch(err => next(err));
+                    } else {
+                        res.statusCode = 200;
+                        res.setHeader('Content-Type', 'text/plain');
+                        res.end('You already have this campsite added to favorites.');
+                    }
+                } else {
+                    Favorite.create({ user: req.user._id, campsites: [req.params.campsiteId] })
+                        .then(favorite => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(favorite);
+                        })
+                        .catch(err => next(err));
+                }
+            })
+            .catch(err => next(err));
+    })
+    .put(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
+        Favorite.findByIdAndUpdate(req.params.campsiteId, {
+            $set: req.body //update operator along with the data in the request body
+        }, { new: true }) //this is so we get back info about the updated document resulting from this method
+            .then(favorite => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(favorite);
+            })
+            .catch(err => next(err));
+    })
+    .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        Favorite.findByIdAndDelete(req.params.campsiteId)
+            .then(response => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(response);
+            })
+            .catch(err => next(err));
+    });
 
 
 module.exports = favoriteRouter;
